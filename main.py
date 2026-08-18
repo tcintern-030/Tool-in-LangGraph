ff15a188e4c574ac3d17b9676e46f544cac58177
Ahmad Mustafa <ahmadmutafa15@gmail.com>
2026-08-18 16:43:07 +0500

    Multi-Step AI Agents with LangGraph

--- main.py
+++ main.py
@@ -2,88 +2,122 @@ import os
 
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import ToolMessage
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+from typing import Annotated
+from typing_extensions import TypedDict
 from tools import tools
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash",
-    temperature=0,
     google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
 llm_with_tools = llm.bind_tools(tools)
 
-tool_dictionary = {
-    tool.name: tool
-    for tool in tools
-}
-
-
-def run_agent(user_question):
-
-    print("\nUser:")
-    print(user_question)
-
-    response = llm_with_tools.invoke(user_question)
-
-    if not response.tool_calls:
-
-        print("\nAgent:")
-        print(response.content)
-
-        return response.content
-
-    tool_messages = []
-
-    for tool_call in response.tool_calls:
-
-        tool_name = tool_call["name"]
-        tool_arguments = tool_call["args"]
-
-        print("\nTool Selected:")
-        print(tool_name)
-
-        print("Arguments:")
-        print(tool_arguments)
-
-        selected_tool = tool_dictionary[tool_name]
-
-        try:
-
-            result = selected_tool.invoke(tool_arguments)
-
-        except Exception as e:
-
-            result = f"Tool failed: {str(e)}"
-
-        print("\nTool Result:")
-        print(result)
-
-        tool_message = ToolMessage(
-            content=result,
-            tool_call_id=tool_call["id"]
-        )
-
-        tool_messages.append(tool_message)
-
-    messages = [
-        response,
-        *tool_messages
+class State(TypedDict):
+    messages: Annotated[
+        list,
+        add_messages
     ]
 
-    final_response = llm_with_tools.invoke(messages)
+def agent_node(state: State):
+    print("---AGENT NODE---")
 
-    print("\nFinal Answer:")
-    print(final_response.content)
+    messages = state["messages"]
+    response = llm_with_tools.invoke(messages)
 
-    return final_response.content
+    print("Agent Response:")
+
+    if response.tool_calls:
+        for tool_call in response.tool_calls:
+            print(
+                "Tool Selected:",
+                tool_call["name"]
+            )
+            print(
+                "Arguments:",
+                tool_call["args"]
+            )
+    else:
+        print(response.content)
+
+    return {
+        "messages": [response]
+    }
+
+tool_node = ToolNode(tools)
+
+def should_continue(state: State):
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    if last_message.tool_calls:
+        print("\nRouting to Tool Node...")
+        return "tools"
+
+    print("\nNo tool required.")
+
+    return END
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("agent", agent_node)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_edge(START,"agent")
+graph_builder.add_conditional_edges(
+    "agent",
+    should_continue,
+    {
+        "tools": "tools",
+        END: END
+    }
+)
+
+graph_builder.add_edge("tools", "agent")
+
+graph = graph_builder.compile()
+
+def run_agent(question):
+    print("---USER---")
+    print(question)
+
+    result = graph.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content=question
+                )
+            ]
+        }
+    )
+
+    final_message = result["messages"][-1]
+
+    print("---FINAL ANSWER---")
+
+    if isinstance(final_message.content, list):
+        for item in final_message.content:
+            if item.get("type") == "text":
+                print(item["text"])
+
+    else:
+        print(final_message.content)
+
 
 if __name__ == "__main__":
 
     while True:
-        question = input("\nAsk something (type exit to stop): ")
+
+        question = input(
+            "\nAsk something "
+            "(type exit to stop): "
+        )
 
         if question.lower() == "exit":
             break
